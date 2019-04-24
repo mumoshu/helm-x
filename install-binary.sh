1b73cf2f31f9be91904ea413eb4183f62968224a
Yusuke KUOKA <ykuoka@gmail.com>
2019-04-24 16:52:59 +0900

    fix plugin installation

--- install-binary.sh
+++ install-binary.sh
@@ -8,17 +8,45 @@ if [ -n "${HELM_PUSH_PLUGIN_NO_INSTALL_HOOK}" ]; then
     exit 0
 fi
 
-version="$(curl -s https://api.github.com/repos/mumoshu/helm-x/releases/latest | awk '/\"tag_name\":/{gsub( /[,\"]/,"", $2); print $2}')"
+# initARch and initOS copied w/ love from https://github.com/technosophos/helm-template
+
+# initArch discovers the architecture for this system.
+initArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv5*) ARCH="armv5";;
+    armv6*) ARCH="armv6";;
+    armv7*) ARCH="armv7";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
+
+# initOS discovers the operating system for this system.
+initOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Msys support
+    msys*) OS='windows';;
+    # Minimalist GNU for Windows
+    mingw*) OS='windows';;
+  esac
+}
+
+initArch
+initOS
+
+VER=$(awk '/version:/{gsub(/\"/,"", $2); print $2}' plugin.yaml)
+version=v${VER}
+#version="$(curl -s https://api.github.com/repos/mumoshu/helm-x/releases/latest | awk '/\"tag_name\":/{gsub( /[,\"]/,"", $2); print $2}')"
 echo "Downloading and installing helm-x ${version} ..."
 
-url=""
-if [ "$(uname)" = "Darwin" ]; then
-    url="https://github.com/mumoshu/helm-x/releases/download/${version}/helm-x-macos-${version}.tgz"
-elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com/mumoshu/helm-x/releases/download/${version}/helm-x-linux-${version}.tgz"
-else
-    url="https://github.com/mumoshu/helm-x/releases/download/${version}/helm-x-windows-${version}.tgz"
-fi
+VER=${version/v/}
+url="https://github.com/mumoshu/helm-x/releases/download/${version}/helm-x_${VER}_${OS}_${ARCH}.tar.gz"
 
 echo $url
 
@@ -32,7 +60,9 @@ if [ -x "$(which curl 2>/dev/null)" ]; then
 else
     wget -q "${url}" -O "releases/${version}.tgz"
 fi
+
+find releases
 tar xzf "releases/${version}.tgz" -C "releases/${version}"
-mv "releases/${version}/inj" "bin/inj" || \
-    mv "releases/${version}/inj.exe" "bin/inj"
+mv "releases/${version}/helm-x" "bin/helm-x" || \
+    mv "releases/${version}/helm-x.exe" "bin/helm-x"
 rm -rf releases
